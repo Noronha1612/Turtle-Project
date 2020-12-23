66b73e72d277b1350c6cad39f932aa7030fbbd39
NoronhaGCP <inc.691@gmail.com>
2020-12-23 00:13:20 -0300

    add celebrate validation to routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,34 @@ const usersController = new UsersController();
 
 const routes = Router();
 
-routes.get('/users', usersController.index);
-routes.post('/users', usersController.create);
+routes.get('/users', celebrate({
+    headers: Joi.object({
+        ids: Joi.string().required()
+    }).options({ allowUnknown: true })
+}), usersController.index);
+
+routes.post('/users', celebrate({
+    body: Joi.object({
+        name: Joi.string().required(),
+        nickname: Joi.string().required(),
+        whatsapp: Joi.string().required(),
+        city: Joi.string().required(),
+        email: Joi.string().required(),
+        avatar_id: Joi.number().required(),
+        password: Joi.string().required(),
+        birthday: Joi.string().required()
+    })
+}), usersController.create);
+
+routes.put('/users', celebrate({
+    body: Joi.object({
+        id: Joi.number().required(),
+        name: Joi.string().required(),
+        whatsapp: Joi.string().required(),
+        city: Joi.string().required(),
+        email: Joi.string().required(),
+        avatar_id: Joi.number().required()
+    })
+}), usersController.updateGenericData);
 
 export default routes;
